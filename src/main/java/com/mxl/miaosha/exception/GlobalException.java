02917b086124d018b3d0ebf86ba60c0440ef90b1
1359533909 <1359533909@qq.com>
2019-10-08 09:00:48 +0800

    提交项目

---
+++ src/main/java/com/mxl/miaosha/exception/GlobalException.java
@@ -0,0 +1,20 @@
+package com.mxl.miaosha.exception;
+
+import com.mxl.miaosha.result.CodeMsg;
+
+public class GlobalException extends RuntimeException{
+
+	private static final long serialVersionUID = 1L;
+	
+	private CodeMsg cm;
+	
+	public GlobalException(CodeMsg cm) {
+		super(cm.toString());
+		this.cm = cm;
+	}
+
+	public CodeMsg getCm() {
+		return cm;
+	}
+
+}
